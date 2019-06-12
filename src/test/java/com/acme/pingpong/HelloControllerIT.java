4b7a7a5bdc42e653d7cd3711369024dc59dfb150
Jonas Lalin <1ac6df24@gmail.com>
2019-06-12 14:48:46 +0200

    add test.war name

--- src/test/java/com/acme/pingpong/HelloControllerIT.java
+++ src/test/java/com/acme/pingpong/HelloControllerIT.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 public class HelloControllerIT {
     @Deployment
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class)
+        return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addClasses(
                         HelloService.class,
                         HelloController.class)
